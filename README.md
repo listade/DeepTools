969858041292a9de7088517e4e76af50b6b11ef5
Egor Sholukh <egor.bulochka@gmail.com>
2022-07-18 23:01:28 +0300

    Add setup.py

---
+++ README.md
@@ -0,0 +1,4 @@
+
+Install
+`pip install git+ssh://git@github.com/connectedplanewifi/detect.git --extra-index-url https://download.pytorch.org/whl/cu116`
+`pip install git+https://github.com/JunnYu/mish-cuda`
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='detect',
+    version='0.0.1',
+    author='Sholukh Egor',
+    author_email='egor.bulochka@gmail.com',
+    packages=['detect'],
+    python_requires='>=3.7',
+    install_requires=[
+        "matplotlib==3.5.2",
+        "numpy==1.21.6",
+        "opencv-python==4.6.0.66",
+        "Pillow==9.2.0",
+        "pyaml==21.10.1",
+        "scipy==1.7.3",
+        "tqdm==4.64.0",
+        "torch",
+        "torchaudio",
+        "torchvision",
+    ],
+)
