6720457cf4515f381c0a5c5751e0a551967b6ee6
Egor Sholukh <egor.bulochka@gmail.com>
2022-07-23 14:43:29 +0300

    Remove config, refactoring

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 
-Install
-
-`pip install git+ssh://git@github.com/connectedplanewifi/detect.git --extra-index-url https://download.pytorch.org/whl/cu116`
-`pip install git+https://github.com/JunnYu/mish-cuda`
-
-Run
-  `python -m detect.infer`
-  
- You should download and modify config default.ini
+Install:
+  `$ pip install git+https://github.com/JunnYu/mish-cuda`
+  `$ pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu116` 
--- detect/inference.py
+++ detect/inference.py
@@ -1,5 +1,4 @@
 import argparse
-import configparser
 import os
 import warnings
 
@@ -13,51 +12,30 @@ from .utils.general import non_max_suppression, plot_one_box, scale_coords
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="default.ini")
-    opt = parser.parse_args()
 
-    config = configparser.ConfigParser()
-    config.read(opt.config)
-
-    net = config["Net"]
-    device = net["Device"]
-
-    pre = config["Pre"]
-    width = int(pre["Width"])
-    overlap = int(pre["Overlap"])
-
-    infer = config["Infer"]
-    input_dir = infer["InputDir"]
-    output_dir = infer["OutputDir"]
-    weights = infer["WeightsPath"]
-    conf_thres = float(infer["ConfThres"])
-    iou_thres = float(infer["IouThres"])
-
-    device = torch.device(device)
-    dataset = LoadImages_(input_dir, img_size=width)
+def inference(opt):
+    device = torch.device(opt.device)
+    dataset = LoadImages_(opt.input, img_size=opt.img_size)
 
     with torch.no_grad():
-        model = torch.load(weights, map_location=device)['model'].float().fuse().eval()  # load FP32 model
+        model = torch.load(opt.weights, map_location=device)['model'].float().fuse().eval()  # load FP32 model
 
         for path, img_np in dataset:
             im_height, im_width = img_np.shape[:-1]
             preds = torch.zeros((0, 6), dtype=torch.float32).to(device)
 
-            for hstep in range(0, im_height, width-overlap):
-                for wstep in range(0, im_width, width-overlap):
-
-                    im_tile = img_np[hstep:hstep+width, wstep:wstep+width, :] # tile
+            for hstep in range(0, im_height, opt.img_size-opt.overlap):
+                for wstep in range(0, im_width, opt.img_size-opt.overlap):
+                    im_tile = img_np[hstep:hstep+opt.img_size, wstep:wstep+opt.img_size, :] # tile
                     tile_height, tile_width = im_tile.shape[:-1]
 
-                    if tile_width < width and tile_height == width:
-                        im_tile = np.pad(im_tile, [(0, 0), (0, width-tile_width), (0, 0)], mode='constant', constant_values=114)
-                    elif tile_height < width and tile_width == width:
-                        im_tile = np.pad(im_tile, [(0, width-tile_height), (0, 0), (0, 0)], mode='constant', constant_values=114)
-                    elif tile_width < width and tile_height < width:
-                        im_tile = np.pad(im_tile, [(0, width-tile_height), (0, width-tile_width), (0, 0)], mode='constant', constant_values=114)
-                    elif tile_width > width or tile_height > width:
+                    if tile_width < opt.img_size and tile_height == opt.img_size:
+                        im_tile = np.pad(im_tile, [(0, 0), (0, opt.img_size-tile_width), (0, 0)], mode='constant', constant_values=114)
+                    elif tile_height < opt.img_size and tile_width == opt.img_size:
+                        im_tile = np.pad(im_tile, [(0, opt.img_size-tile_height), (0, 0), (0, 0)], mode='constant', constant_values=114)
+                    elif tile_width < opt.img_size and tile_height < opt.img_size:
+                        im_tile = np.pad(im_tile, [(0, opt.img_size-tile_height), (0, opt.img_size-tile_width), (0, 0)], mode='constant', constant_values=114)
+                    elif tile_width > opt.img_size or tile_height > opt.img_size:
                         raise ValueError('dimension mismatch: cropped image size')
 
                     init_im_size = im_tile.shape[:-1]  # initial crop size 1024x1024
@@ -79,7 +57,9 @@ if __name__ == '__main__':
                     pred = model(im_tile, augment=False)[0]
 
                     # Apply NMS
-                    pred = non_max_suppression(pred, conf_thres=conf_thres, iou_thres=iou_thres)
+                    pred = non_max_suppression(pred,
+                                               conf_thres=opt.conf_thres,
+                                               iou_thres=opt.iou_thres)
 
                     if pred[0] is not None:
                         # rescaling coordinates from 640 to 1024
@@ -100,9 +80,26 @@ if __name__ == '__main__':
             dets = dets.cpu().numpy()
             dets[:, [4, 5]] = dets[:, [5, 4]]
 
-            file = os.path.join(output_dir, os.path.basename(path))
+            file = os.path.join(opt.output, os.path.basename(path))
             np.savetxt(file + '.txt', dets, fmt=('%d', '%d', '%d', '%d', '%d', '%1.3f')) # x1 y1 x2 y2 cls score
 
             for v in dets:
                 plot_one_box(v[:4], img_np, line_thickness=1)
             cv2.imwrite(file, img_np)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input", type=str, default="input")
+    parser.add_argument("--output", type=str, default="output")
+    parser.add_argument("--weights", type=str, default="yolov4-p5.pt")
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--conf-thres", type=float, default=0.5)
+    parser.add_argument("--iou-thres", type=float, default=0.1)
+    parser.add_argument("--img-size", type=int, default=640)
+    parser.add_argument("--overlap", type=int, default=100)
+
+    opt = parser.parse_args()
+
+    inference(opt)
