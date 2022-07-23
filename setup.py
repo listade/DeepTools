117068b8f80c6c25d1161083a7803f517bd42ef8
Egor Sholukh <egor.bulochka@gmail.com>
2022-07-23 13:31:57 +0300

    Add requirements.txt

--- setup.py
+++ setup.py
@@ -7,16 +7,5 @@ setup(
     author_email="egor.bulochka@gmail.com",
     packages=find_packages(),
     python_requires=">=3.7",
-    install_requires=[
-        "matplotlib==3.5.2",
-        "numpy==1.21.6",
-        "opencv-python==4.6.0.66",
-        "Pillow==9.2.0",
-        "pyaml==21.10.1",
-        "scipy==1.7.3",
-        "tqdm==4.64.0",
-        "torch",
-        "torchaudio",
-        "torchvision",
-    ],
+    install_requires=open("requirements.txt").read().split("\n"),
 )
