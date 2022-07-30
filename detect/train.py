618cb80a678034876ad3888f67db8dddb2120c7f
Egor Sholukh <egor.bulochka@gmail.com>
2022-07-30 18:36:40 +0300

    Set detect.train default param

--- detect/train.py
+++ detect/train.py
@@ -376,7 +376,7 @@ def train(hyp, opt, device, tb_writer=None):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='yolov4-p5.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
+    parser.add_argument('--cfg', type=str, default='yolov4-p5.yaml', help='model.yaml path')
     parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
     parser.add_argument('--hyp', type=str, default='', help='hyperparameters path, i.e. data/hyp.scratch.yaml')
     parser.add_argument('--epochs', type=int, default=300)
