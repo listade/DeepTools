1c490a81ecc42ce939c104591eeeac3f832c4473
Egor Sholukh <egor.bulochka@gmail.com>
2022-08-07 18:42:06 +0300

    Add test.bat

---
+++ test.bat
@@ -0,0 +1,10 @@
+
+call "%~1\env\Scripts\activate.bat"
+
+set PYTHONPATH="%~1"
+
+echo "Running detection.."
+python -m app.detect.inference --input "%~1\input" --weights "%~1\weights\car.pt" --save-img
+
+echo "Running segmentation.."
+python -m app.semantic.inference --input "%~1\input" --weights "%~1\weights\lake.ckpt" 
